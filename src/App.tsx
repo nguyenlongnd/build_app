9896a0769e5b22a14d14e88515d3ee993295c81d
longnguyen <nguyenlong@gmail.com>
2024-02-06 08:32:15 +0700

    long

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import DashboardMessages from './components/DashboardMessages/DashboardMessages';
+import DashboardTasks from './components/DashboardTasks/DashboardTasks';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Dashboard />}>
+        <Route path="/messages" element={<DashboardMessages />} />
+        <Route path="tasks" element={<DashboardTasks />} />
+      </Route>
+      <Route path="/messages2" element={<DashboardMessages />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
